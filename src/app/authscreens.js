d9c702b85d6838e2bfff0550506cf92a30d19d0c
= <=>
2021-03-16 22:58:28 -0700

    merger to main

--- src/app/authscreens.js
+++ src/app/authscreens.js
@@ -49,8 +49,9 @@ const Register = () => {
 const Test = () => {
     return (
         <Center>
-            <LanguagePost/>
-            <Language/>
+            {/* <LanguagePost/>
+            <Language/> */}
+            <Profile></Profile>
         </Center>
     )
 }
